a717a1adf7ffd8bb1e7ff386cc31fada3dff3842
Muhammad Yahya <zulfikeryahya@gmail.com>
2023-09-16 12:02:17 +0600

    eamaJhon client from DB

--- src/components/Routes/PrivateRoute.jsx
+++ src/components/Routes/PrivateRoute.jsx
@@ -5,7 +5,7 @@ import { Navigate, useLocation } from 'react-router-dom';
 const PrivateRoute = ({children}) => {
     const {user, loading} = useContext(AuthContext);
     const location = useLocation()
-    console.log(location)
+   
     if(loading){
         return <div>Loading...</div>
     }
